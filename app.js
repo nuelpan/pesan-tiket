c7644505eb0287115ce6bb2a89c49fc9cbf841fc
Immanuel Pandiangan <nuelpan@gmail.com>
2019-11-20 11:55:09 +0700

    add user router

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
 const express = require('express');
 const app = express();
-const routerRoute = require('./routers/routerRoute');
-
+const admin = require('./routers/admin');
+const user = require('./routers/user')
 // set view engine using ejs
 app.set('view-engine', 'ejs');
 
-// set router route root url
-app.use('/route', routerRoute);
+// set router route root 
+app.use('/user', user);
+app.use('/admin', admin);
 
 app.listen(3000, () => {
     console.log('app listen on port 3000');
