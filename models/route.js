8816793280547e9cdd285f842cb3c8c29122d461
Immanuel Pandiangan <nuelpan@gmail.com>
2019-11-21 16:26:24 +0700

    add validate add rout

--- models/route.js
+++ models/route.js
@@ -1,10 +1,19 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {  
-  const Route = sequelize.define('Route', {
+module.exports = (sequelize, DataTypes) => {
+  const Model = sequelize.Sequelize.Model
+  class Route extends Model {}  
+  Route.init({
     from: DataTypes.STRING,
     to: DataTypes.STRING,
     price: DataTypes.REAL
-  }, {});
+  }, {hooks: {
+    beforeCreate: (instance) => {
+      if (instance.from === "" || instance.to === "" || instance.price === "") {
+        // console.log(instance.from === "")
+        return new Error('Field must not empty')
+      }
+    }
+  } ,sequelize});
   Route.associate = function(models) {
     // associations can be defined here
     Route.belongsToMany(models.User, {through: models.Ticket})
--- routers/admin.js
+++ routers/admin.js
@@ -9,11 +9,11 @@ router.use(express.urlencoded({extended: true}));
 
 // Input Route
 router.get('/', controllerAdmin.viewHome);
-router.post('/', controllerAdmin.createRoute);
 router.get('/:id/edit', controllerAdmin.viewEditRoute)
-router.post('/:id/edit', controllerAdmin.saveEditRoute)
 router.get('/:id/delete', controllerAdmin.deleteRoute)
-
 router.get('/tickets', controllerAdmin.viewTickets)
+
+router.post('/', controllerAdmin.createRoute);
+router.post('/:id/edit', controllerAdmin.saveEditRoute)
 
 module.exports = router;
